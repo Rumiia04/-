ee4c1d46eebe57743f6cc88c22230c27595ece42
Rumia <147132163+Rumiia04@users.noreply.github.com>
2024-05-08 11:37:13 +0300

    Add files via upload

---
+++ 07.05.2024/07.05.2024/07.05.2024.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main(){
+    setlocale(LC_ALL, "Russian");
+
+    string line = "Informationbooks.txt";
+
+    ofstream fout;
+
+    fout.open(line);
+
+    if (!fout.is_open())
+    {
+        cout << "Error opening file!\n";
+    }
+    else
+    {
+        fout << "Война и мир  Лев Толстой  1869  Роман \n1984  Джордж Оруэлл  1949  Фантастика\nГарри Поттер и философский камень  Джоан Роулинг  1997  Фэнтези\n";
+        fout << "Алиса в стране чудес  Льюис Кэрролл  1865  Фэнтези\nКапитанская дочка  Александр Пушкин  1836  Роман";
+    }
+    fout.close();
+}
